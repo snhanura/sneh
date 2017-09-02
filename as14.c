f73cfa357093a657b599c8a1ea5dc4e670f97f50
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-02 03:37:50 -0700

    Create as14.c

---
+++ as14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int start,end;
+printf("enter the values");
+scanf("%d%d",&start,&end);
+for(i=start;i<=end;i++)
+{
+odd=i%2;
+if(odd!=0)
+{
+printf("given number is odd%d",i);
+}}
