56a60e3e66e71a492cc9402e6c743ff2366be407
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-02 03:47:20 -0700

    Create as16.c

---
+++ as16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i start,end;
+printf("enter the values");
+scanf("%d%d"&start,&end);
+for(i=start;i<end/2;++i)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+if(falg==0)
+{
+printf("it is a prime number");
+}
+else
+{
+printf("it is not a prime number");
+}}
