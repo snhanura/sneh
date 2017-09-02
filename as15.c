328e9415b894b56c81b37ddabd6dc8d1ad960633
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-02 03:41:22 -0700

    Create as15.c

---
+++ as15.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int start,end;
+printf("enter the values");
+scanf("%d%d",&start,&end);
+for(i=start;i<=end;i++)
+{
+even=i%2;
+if(even==0)
+{
+printf("given number is even%d",i);
+}}
